f5f62273c8beec82ffc431cd9aa683edffc2e06a
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-06-26 23:20:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/phonebook">
       <Provider store={store}>
         <App />
       </Provider>
